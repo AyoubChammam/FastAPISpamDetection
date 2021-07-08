96b6effd5db662e3cc961a5435e1249c4fe54a03
AyoubChammam <ayoubchamam12@gmail.com>
2021-07-08 13:50:33 +0100

    Run app

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
 
@@ -11,3 +11,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == '__main__':
+    uvicorn.run(app)
--- app/train_spam_detector.py
+++ app/train_spam_detector.py
@@ -25,11 +25,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 # Training a Neural Network Pipeline
 
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import cross_val_score
 
 tfidf = TfidfVectorizer(strip_accents=None, lowercase=False,
                         max_features=700,
